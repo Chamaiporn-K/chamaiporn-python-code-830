7563213d92b1e83eb0b6d5da0c676fc837dfea7a
Chamaiporn Konglae <chamaiporn.kon@ku.th>
2025-07-22 15:22:54 +0700

    ย้าย

--- assignments/week01/homework.py
+++ assignments/week01/homework.py
@@ -2,15 +2,4 @@ monthly_income = float(input("User's monthly income in THB : "))
 rent_cost = float(input("Monthly rent/housing cost : "))
 transportation_cost = float(input("Monthly transportation expenses : "))
 entertainment_budget = int(input("Monthly entertainment budget : "))
-emergency_fund_percent = float(input("Percentage to save for emergency : ")/    44)
-
-
-
-number = 2                                                                                                                                                                                             
-for number in range(2,10) :
-    if number%2 == 0:
-        print("the number "+str(number)+" is even")
-    else :
-        print("The number "+str(number)+" is odd")
-    number = number+1
-
+emergency_fund_percent = float(input("Percentage to save for emergency : "))
